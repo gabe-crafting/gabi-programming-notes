3410ff9a2ec02089be23ea5048a06bdc279d6f99
gabe-crafting <gabriel.projects90@proton.me>
2025-09-29 15:30:20 +0000

    Deploying to gh-pages from @ gabe-crafting/gabi-programming-notes@3fdf65b2d93d3258ece820283fa74c686c4054f5 🚀

---
+++ _nuxt/h2u6vX4-.js
@@ -0,0 +1,1 @@
+import{r as n}from"./DT8DxhIH.js";import a from"./Bc0mBTtf.js";import{z as s,H as t,i as e,w as i,W as p}from"./DvT5QwBX.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
